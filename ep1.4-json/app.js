dbc49859e38d15d09ac4fad02f832282ddc6aca6
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-03-15 16:22:17 +0700

    JSON

---
+++ ep1.4-json/app.js
@@ -0,0 +1,15 @@
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const {latitude, longitude} = data;
+
+    document.getElementById('lat').textContent = latitude;
+    document.getElementById('lon').textContent = longitude;
+
+    console.log(latitude);
+    console.log(longitude);
+}
+
+setInterval(getISS, 2000)
